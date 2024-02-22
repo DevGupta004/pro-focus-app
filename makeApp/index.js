01c5d0b481aae8f221e7a5f2c1e3353c37af9127
Dev <dev@Devs-MacBook-Air.local>
2024-02-22 18:27:35 +0530

    modify aab

--- makeApp/index.js
+++ makeApp/index.js
@@ -5,20 +5,26 @@ const data = require('./dummy-data/appData.json');
 init();
 
 async function init() {
-  const {appName, packageName} = data;
-  console.log('data', appName, packageName);
-  if (appName && packageName) {
-    await buildApp(appName,packageName );
+  try {
+    const {appName, packageName} = data;
+    console.log('data', appName, packageName);
+    if (appName && packageName) {
+      await buildApp(appName,packageName);
+    }
+  } catch (error) {
+    console.log("Error Inside init fun", error);
   }
 }
 
 async function buildApp(appName, packageName) {
-
+  try {
     commandArr = [
-        `./AppBuilder.sh`,
-        `--appName`, `${appName}`,
-        `--packageName`, `${packageName}`,
+      `./AppBuilder.sh`,
+      `--appName`, `${appName}`,
+      `--packageName`, `${packageName}`,
     ];
-
     await RunShScript(commandArr);
+  } catch (error) {
+    console.log("error inside build App", error);
+  }
 }
